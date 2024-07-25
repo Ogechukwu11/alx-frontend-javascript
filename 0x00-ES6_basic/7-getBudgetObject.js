bf598d5c3d6cefbcee735ef2d972a8ec6dc9bd7c
Ogechukwu11 <nwobumogechukwu@gmail.com>
2024-07-25 17:42:41 +0100

    Object property value shorthand syntax

---
+++ 0x00-ES6_basic/7-getBudgetObject.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+/* eslint-disable no-unused-vars */
+/* eslint-disable */
+export default function getBudgetObject(income, gdp, capita) {
+  const budget = {
+    income, gdp, capita
+  };
+
+  return budget;
+}
