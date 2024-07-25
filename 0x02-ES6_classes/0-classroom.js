ae89ae571695c700fb8c50a410cbe9458e78d03c
Ogechukwu11 <nwobumogechukwu@gmail.com>
2024-07-25 23:23:35 +0100

    You used to attend a place like this at some point

---
+++ 0x02-ES6_classes/0-classroom.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+/* eslint-disable no-unused-vars */
+/* eslint-disable */
+export default class ClassRoom {
+  constructor(maxStudentsSize) {
+    this._maxStudentsSize = maxStudentsSize;
+  }
+}
