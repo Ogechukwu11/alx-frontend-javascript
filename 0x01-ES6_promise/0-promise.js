019d03cecc4dfb174946d4f50703cc62e7915ee2
Ogechukwu11 <nwobumogechukwu@gmail.com>
2024-07-27 10:36:31 +0100

    Keep every promise you make and only make promises you can keep

---
+++ 0x01-ES6_promise/0-promise.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+/* eslint-disable no-unused-vars */
+/* eslint-disable */
+export default function getResponseFromAPI() {
+  return new Promise((resolve, reject) => {
+    resolve();
+  });
+}
