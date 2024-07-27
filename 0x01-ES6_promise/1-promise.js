cae088e22cced796f5af1d1fe583c3fcc6990b1e
Ogechukwu11 <nwobumogechukwu@gmail.com>
2024-07-27 10:55:44 +0100

    Don't make a promise...if you know you can't keep it

--- 0x01-ES6_promise/1-promise.js
+++ 0x01-ES6_promise/1-promise.js
@@ -7,7 +7,7 @@ export default function getFullResponseFromAPI(success) {
       resolve({ status: 200, body: 'Success' });
     }
     else {
-      reject("The fake API is not working currently");
+      reject(new Error("The fake API is not working currently"));
     }
   });
 }
