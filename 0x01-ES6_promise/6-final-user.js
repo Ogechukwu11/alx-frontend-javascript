ce0ebb9bebee14a3f0ab057ecf8429ea4c42cb3e
Ogechukwu11 <nwobumogechukwu@gmail.com>
2024-07-28 11:10:45 +0100

    Handle multiple promises

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -4,25 +4,24 @@
 import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
-export default function handleProfileSignup(firstName, lastName, fileName) {
-  return Promise.allSettled([
-    signUpUser(firstName, lastName),
-    uploadPhoto(fileName)
-  ])
-    .then((results) => {
-      return results.map((result) => {
-        if (result.status === 'fulfilled') {
-	  return {
-            status: 'fulfilled',
-            value: result.value
-	  };
-	}
-         else {
-	   return {
-             status: 'rejected',
-             value: result.reason
-	   };
-	 }
-      });
-    });
+export default async function handleProfileSignup(
+  firstName, lastName, fileName,
+) {
+  const result = [];
+  try {
+    const signup = await signUpUser(firstName, lastName);
+    result.push({ status: 'fulfilled', value: signup }); 
+  }
+  catch (error) {
+    result.push({ status: 'rejected', value: error });   
+  }
+
+  try {
+    const upload = await uploadPhoto(filename);
+    result.push({ status: 'fulfilled', value: upload });
+  }
+  catch (error) {
+    result.push({ status: 'rejected', value: error });
+  }
+  return result;
 }
