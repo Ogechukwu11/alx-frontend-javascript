1771bcf4518303052e4242cdb4189be8be48fe26
Ogechukwu11 <nwobumogechukwu@gmail.com>
2024-07-28 13:20:05 +0100

    Throw error / try catch

---
+++ 0x01-ES6_promise/8-try.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+/* eslint-disable no-unused-vars */
+/* eslint-disable */
+export default function divideFunction(numerator, denominator) {
+  if (denominator === 0) {
+    throw new Error('cannot divide by 0');
+  }
+  else {
+    return (numerator / denominator);
+  }
+}
