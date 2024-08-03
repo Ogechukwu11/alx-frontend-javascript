cbf6a824f3b240271925f033c1074f4c32115f25
Ogechukwu11 <nwobumogechukwu@gmail.com>
2024-08-03 03:59:01 +0100

    More mapping

--- 0x03-ES6_data_manipulation/1-get_list_student_ids.js
+++ 0x03-ES6_data_manipulation/1-get_list_student_ids.js
@@ -5,5 +5,7 @@ export default function getListStudentIds(students) {
   if (!Array.isArray(students)) {
     return {};
   }
-  return students.map(student => student.id);
+  return students.map(student => {
+    return student['id'];
+  });
 }
